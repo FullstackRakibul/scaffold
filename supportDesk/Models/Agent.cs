4f4a2c1da4147e4564ae59bd6fa97518512ccc62
FullstackRakibul <rakibul.cse.47@gmail.com>
2023-11-18 14:10:43 +0600

    scaffold project

---
+++ supportDesk/Models/Agent.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using supportDesk.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace supportDesk.Models;
+
+public partial class Agent
+{
+    public int Id { get; set; }
+
+    public Guid? Uid { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string? Phone { get; set; }
+
+    public string? Mail { get; set; }
+
+    public string? Designation { get; set; }
+}
+
+
+public static class AgentEndpoints
+{
+	public static void MapAgentEndpoints (this IEndpointRouteBuilder routes)
+    {
+        var group = routes.MapGroup("/api/Agent");
+
+        group.MapGet("/", async (SupportdeskdbContext db) =>
+        {
+            return await db.Agents.ToListAsync();
+        })
+        .WithName("GetAllAgents")
+        .Produces<List<Agent>>(StatusCodes.Status200OK);
+
+        group.MapGet("/{id}", async  (int id, SupportdeskdbContext db) =>
+        {
+            return await db.Agents.AsNoTracking()
+                .FirstOrDefaultAsync(model => model.Id == id)
+                is Agent model
+                    ? Results.Ok(model)
+                    : Results.NotFound();
+        })
+        .WithName("GetAgentById")
+        .Produces<Agent>(StatusCodes.Status200OK)
+        .Produces(StatusCodes.Status404NotFound);
+
+        group.MapPut("/{id}", async  (int id, Agent agent, SupportdeskdbContext db) =>
+        {
+            var affected = await db.Agents
+                .Where(model => model.Id == id)
+                .ExecuteUpdateAsync(setters => setters
+                  .SetProperty(m => m.Id, agent.Id)
+                  .SetProperty(m => m.Uid, agent.Uid)
+                  .SetProperty(m => m.Name, agent.Name)
+                  .SetProperty(m => m.Phone, agent.Phone)
+                  .SetProperty(m => m.Mail, agent.Mail)
+                  .SetProperty(m => m.Designation, agent.Designation)
+                  );
+            return affected == 1 ? Results.Ok() : Results.NotFound();
+        })
+        .WithName("UpdateAgent")
+        .Produces(StatusCodes.Status404NotFound)
+        .Produces(StatusCodes.Status204NoContent);
+
+        group.MapPost("/", async (Agent agent, SupportdeskdbContext db) =>
+        {
+            db.Agents.Add(agent);
+            await db.SaveChangesAsync();
+            return Results.Created($"/api/Agent/{agent.Id}",agent);
+        })
+        .WithName("CreateAgent")
+        .Produces<Agent>(StatusCodes.Status201Created);
+
+        group.MapDelete("/{id}", async  (int id, SupportdeskdbContext db) =>
+        {
+            var affected = await db.Agents
+                .Where(model => model.Id == id)
+                .ExecuteDeleteAsync();
+            return affected == 1 ? Results.Ok() : Results.NotFound();
+        })
+        .WithName("DeleteAgent")
+        .Produces<Agent>(StatusCodes.Status200OK)
+        .Produces(StatusCodes.Status404NotFound);
+    }
+}
---
+++ supportDesk/Models/SupportdeskdbContext.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace supportDesk.Models;
+
+public partial class SupportdeskdbContext : DbContext
+{
+    public SupportdeskdbContext()
+    {
+    }
+
+    public SupportdeskdbContext(DbContextOptions<SupportdeskdbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Agent> Agents { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=localhost\\MSSQLSERVER01;Initial Catalog=supportdeskdb;Integrated Security=True; Encrypt=True;TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Agent>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__agents__3213E83F1F511ACC");
+
+            entity.ToTable("agents");
+
+            entity.HasIndex(e => e.Name, "idx_name");
+
+            entity.Property(e => e.Id)
+                .ValueGeneratedNever()
+                .HasColumnName("id");
+            entity.Property(e => e.Designation)
+                .HasMaxLength(100)
+                .HasColumnName("designation");
+            entity.Property(e => e.Mail)
+                .HasMaxLength(255)
+                .HasColumnName("mail");
+            entity.Property(e => e.Name)
+                .HasMaxLength(255)
+                .HasColumnName("name");
+            entity.Property(e => e.Phone)
+                .HasMaxLength(15)
+                .HasColumnName("phone");
+            entity.Property(e => e.Uid)
+                .HasDefaultValueSql("(newid())")
+                .HasColumnName("uid");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
